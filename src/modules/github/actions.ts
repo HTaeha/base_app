86f097463eec516b4f56991f5807f90174c50001
HTaeha <patterson@linecare.net>
2021-07-27 17:32:55 +0900

    thunk adding...

---
+++ src/modules/github/actions.ts
@@ -0,0 +1,19 @@
+import { createAction, createAsyncAction } from "typesafe-actions"
+import { AxiosError } from "axios"
+
+import { GithubProfile } from "../../api/github"
+import { createStandardAction } from "typesafe-actions/dist/deprecated/create-standard-action"
+
+export const GET_USER_PROFILE = "github/GET_USER_PROFILE"
+export const GET_USER_PROFILE_SUCCESS = "github/GET_USER_PROFILE_SUCCESS"
+export const GET_USER_PROFILE_ERROR = "github/GET_USER_PROFILE_ERROR"
+
+export const getUserProfile = createStandardAction(GET_USER_PROFILE)()
+export const getUserProfileSuccess = createAction(GET_USER_PROFILE_SUCCESS)<GithubProfile>()
+export const getUserProfileError = createAction(GET_USER_PROFILE_ERROR)<AxiosError>()
+
+export const getUserProfileAsync = createAsyncAction(
+	GET_USER_PROFILE,
+	GET_USER_PROFILE_SUCCESS,
+	GET_USER_PROFILE_ERROR
+)<any, GithubProfile, AxiosError>()
